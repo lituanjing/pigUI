e85d38a7f05a2a4f054947b5de7f04101b97151b
lituanjing <tuanjing@outlook.com>
2020-05-06 16:33:04 +0800

    配置 html-webpack-plugin 打包 index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -18,5 +19,11 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'pigUI - React',
+      template: "index.html"
+    })
+  ]
 }
